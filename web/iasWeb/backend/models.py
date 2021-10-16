1e4f97d88a82abc542bf838248cca1936d07ea08
marticanyelles <marticanyelles@gmail.com>
2021-10-16 15:58:18 +0200

    dabase encryption like a pro

--- web/iasWeb/backend/models.py
+++ web/iasWeb/backend/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.conf import settings
+from mirage import fields
+from mirage.fields import EncryptedCharField
 
 class Folder(models.Model):
-    name = models.CharField(max_length=20)
+    #name = models.CharField(max_length=20)
+    name = EncryptedCharField()
 
 class UserInFolder(models.Model):
     class Meta:
@@ -12,6 +15,7 @@ class UserInFolder(models.Model):
     role = models.IntegerField()
 
 class File(models.Model):
-    name = models.CharField(max_length=50)
+    #name = models.CharField(max_length=50)
+    name = EncryptedCharField()
     path = models.CharField(max_length=100)
     directory = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True)
