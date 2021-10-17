8c055ff9c4601e96424196d634d75a32c8497886
Manel <manel.gracia.sola@gmail.com>
2021-10-17 12:21:06 +0200

    Fixed logout feature

--- web/iasWeb/iasWeb/views/openid.py
+++ web/iasWeb/iasWeb/views/openid.py
@@ -2,19 +2,16 @@
 from mozilla_django_oidc.views import OIDCLogoutView
 from django.conf import settings
 from django.http.response import Http404, HttpResponseRedirect
-import logging
-
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
+from django.contrib.auth import logout
 
 
 def keycloak_logout(request):
     """ Ths method is used to retrieve logout endpoint to also end the
         keycloak session as well as the Django session.
     """
+    logout(request)
     logout_endpoint = settings.OIDC_OP_LOGOUT_ENDPOINT + "?redirect_uri=" + \
                       request.build_absolute_uri(settings.LOGOUT_REDIRECT_URL)
-    logger.warning(logout_endpoint)
     return HttpResponseRedirect(logout_endpoint)
 
 
@@ -23,15 +20,4 @@ class LogoutView(OIDCLogoutView):
     """
 
     def get(self, request):
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
-        logger.warning("h")
         return self.post(request)
